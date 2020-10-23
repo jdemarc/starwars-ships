57d63b2e6b613cf27789cb992db6dfe14804dd5a
joseph <josephjohndemarco@gmail.com>
2020-10-23 17:28:45 -0400

    Add small amount of styling. Successfully fetch from SWAPI. Refactor components and files.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import * as shipAPI from './services/sw-api';
+
+import Header from './components/Header/'
 
 const App = () => {
+  const [starships, setStarships] = useState([]);
+
+  // Cannot use promise or async with useEffect. Rather, use a function instead called within useEffect.
+  useEffect(() => {
+    const fetchShips = async () => {
+      const fetchedData = await shipAPI.getAllStarships();
+
+      setStarships(fetchedData);
+    }
+
+    fetchShips();
+  }, [])
+
+
   return (
-    <div>Hi!</div>
+    <div>
+      <Header />
+    </div>
   );
 }
 
---
+++ src/services/sw-api.js
@@ -0,0 +1,6 @@
+const BASE_URL = 'https://swapi.dev/api/starships'
+
+export const getAllStarships = () => {
+  return fetch(BASE_URL)
+  .then(res => res.json());
+}
