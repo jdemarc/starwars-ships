b4b855673d4e905f788dfc1f62214b751458d904
joseph <josephjohndemarco@gmail.com>
2020-10-23 16:29:38 -0400

    Clean up React bootstrap code. Lay skeleton for pages and components.

---
+++ services/sw-api.js
@@ -0,0 +1,2 @@
+const BASE_URL = 'https://swapi.dev/api/starships'
+
---
+++ src/Pages/StarshipPage/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const StarshipPage = () => {
+  return (
+    <div>Starship Page</div>
+  )
+}
+
+export default StarshipPage;
